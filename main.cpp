aee8bf2f56c66cb22e238fbad7ed8c0dce01fd81
Bernardo Damian Rivera Fernandez <2430254@upv.edu.mx>
2025-09-23 02:05:58 -0600

    se agrego la funcion menu, en donde el usuario podra seleccionar la operacion que necesite, asi como el pedir tambien el archivo csv(aun no terminado)

--- main.cpp
+++ main.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 using namespace std;
 
+string *nombres, *paises, *disciplinas, *generos;
+int *medallas;
+int totalAtletas = 0;
+void menu();
+ int opcion;
+    do {
+        cout << "\n MENU " << endl;
+        cout << "1. Buscar atleta" << endl;
+        cout << "2. Medallas por país" << endl;
+        cout << "3. Atleta con más medallas" << endl;
+        cout << "4. Salir" << endl;
+        cin >> opcion;
+
+        switch(opcion) {
+            case 1: cout << "Buscar atleta (pendiente)\n"; break;
+            case 2: cout << "Medallas por país (pendiente)\n"; break;
+            case 3: cout << "Atleta con más medallas (pendiente)\n"; break;
+            case 4: cout << "Saliendo...\n"; break;
+            default: cout << "Opción inválida\n";
+        }
+    } while(opcion != 4);
+}
+
 int main() {
-    string archivo = "data1.csv";
-    
-    ifstream file(archivo);
-    
-    if (!file.is_open()) {
-        cout << "NO se pudo abrir el archivo " << archivo << endl;
-    } else {
-        cout << "SI se pudo abrir el archivo " << archivo << endl;
-        
-        string linea;
-        getline(file, linea);  // Leer primera línea
-        
-        int pos = linea.find(',');  // Encontrar primera coma
-        string primerNombre = linea.substr(0, pos);  // Extraer antes de la coma
-        
-        cout << "Primer nombre: " << primerNombre << endl;
-        
-        file.close();
-    }
-    
+    string archivo;
+    cout << "Ingrese nombre del archivo: ";
+    cin >> archivo;
     return 0;
 }
